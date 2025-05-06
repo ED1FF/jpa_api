d52d5e7921628723612d234a78508b5846c7a58f
ED1FF <alekarp11@gmail.com>
2025-05-06 23:46:09 +0300

    add tortoise orm code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from typing import Union
-
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# Register Tortoise ORM with PostgreSQL
+register_tortoise(
+    app,
+    db_url="postgres://postgres:postgres@db:5432/jpa_development",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=100)
+    email = fields.CharField(max_length=100, unique=True)
+
+    class Meta:
+        table = "users"
