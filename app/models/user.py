86e1374519392606da6e924cb9e2dc7856a2f3ef
ED1FF <alekarp11@gmail.com>
2025-05-08 23:06:45 +0300

    add password fileds and indexes

--- app/models/user.py
+++ app/models/user.py
@@ -4,8 +4,9 @@ from tortoise.models import Model
 
 class User(Model):
     id = fields.UUIDField(pk=True, default=uuid.uuid4)
-    username = fields.CharField(max_length=50, unique=True)
-    email = fields.CharField(max_length=50, unique=True)
+    username = fields.CharField(max_length=50, unique=True, index=True)
+    email = fields.CharField(max_length=254, unique=True, index=True)
+    password = fields.CharField(max_length=128)
 
     class Meta:
         table = "users"
