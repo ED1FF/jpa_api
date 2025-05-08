ca7648a639f9411bd4dffc525eba9369c113b05a
ED1FF <alekarp11@gmail.com>
2025-05-08 23:10:25 +0300

    init migration

---
+++ app/models/migrations/models/0_20250508200747_init.py
@@ -0,0 +1,24 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "users" (
+    "id" UUID NOT NULL PRIMARY KEY,
+    "username" VARCHAR(50) NOT NULL UNIQUE,
+    "email" VARCHAR(254) NOT NULL UNIQUE,
+    "password" VARCHAR(128) NOT NULL
+);
+CREATE INDEX IF NOT EXISTS "idx_users_usernam_266d85" ON "users" ("username");
+CREATE INDEX IF NOT EXISTS "idx_users_email_133a6f" ON "users" ("email");
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
